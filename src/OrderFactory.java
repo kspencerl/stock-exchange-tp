804bf1f84083497219ea32cec333d9c9a80e2323
Kimberly Liz Spencer Lourenço <127994053+kspencerl@users.noreply.github.com>
2023-12-13 21:52:05 -0300

    Update OrderFactory.java

--- src/OrderFactory.java
+++ src/OrderFactory.java
@@ -1,4 +1,7 @@
 public class OrderFactory {
+    /* 
+    Realiza construção de uma Ordem
+    */
     public static Order createOrder(int quantity, double price, Broker broker, boolean isBuyOrder) {
         return new Order(quantity, price, broker, isBuyOrder);
     }
