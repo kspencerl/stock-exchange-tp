b15c3aed85ebb80dbba31cd8ac2b5bb4515fbf6e
Kimberly Liz Spencer Lourenço <127994053+kspencerl@users.noreply.github.com>
2023-12-13 21:52:33 -0300

    Update TransactionalFactory.java

--- src/TransactionalFactory.java
+++ src/TransactionalFactory.java
@@ -1,4 +1,7 @@
 public class TransactionalFactory {
+    /*
+    Realiza construção de uma transação
+    */
     public static Transactional createTransaction(int quantity, double price) {
         return new Transactional(quantity, price);
     }
