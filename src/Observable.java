c0623c3052338bce563e291e070eeafc23be2e00
Kimberly Liz Spencer Lourenço <127994053+kspencerl@users.noreply.github.com>
2023-12-13 21:40:12 -0300

    Update Observable.java

--- src/Observable.java
+++ src/Observable.java
@@ -1,3 +1,4 @@
+//Interface Observable que define métodos para adiconar um observador, remover observador e notificar os observadores*/
 public interface Observable {
     void addObserver(Observer observer);
     void removeObserver(Observer observer);
